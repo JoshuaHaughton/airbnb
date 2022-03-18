36eac18470252bc27b142f766e61fa20e0942d92
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-03-18 06:06:36 -0400

    Add input and buttons for search functionality

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => {
   const [searchInput, setSearchInput] = useState('');
   const [startDate, setStartDate] = useState(new Date())
   const [endDate, setEndDate] = useState(new Date())
+  const [numberOfGuests, setNumberOfGuests] = useState(1)
 
   const selectionRange = {
     startDate,
@@ -19,6 +20,10 @@ const Header = () => {
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate)
+  }
+
+  const resetInput = () => {
+    setSearchInput('');
   }
 
   return (
@@ -61,6 +66,16 @@ const Header = () => {
         rangeColors={['#FD5B61']}
         onChange={handleSelect}
         />
+        <div className="flex items-center border-b mb-4">
+          <h2 className="text-2xl pl-2 flex-grow font-semibold">Number of Guests</h2>
+
+          <UsersIcon className="h-5" />
+          <input type='number' className="w-12 pl-2 text-lg outline-none text-red-400" value={numberOfGuests} onChange={e => setNumberOfGuests(e.target.value)} min={1}/>
+        </div>
+        <div className="flex">
+          <button className="flex-grow tet-gray-500" onClick={resetInput}>Cancel</button>
+          <button className="flex-grow text-red-400">Search</button>
+        </div>
       </div>
       }
     </header>
